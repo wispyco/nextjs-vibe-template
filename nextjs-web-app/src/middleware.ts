1741bef71683d1696f376de555a74d16f097ca6b
AJ <yspdev@gmail.com>
2025-03-11 14:28:27 -0700

    fix middleware

--- nextjs-web-app/src/middleware.ts
+++ nextjs-web-app/src/middleware.ts
@@ -8,12 +8,13 @@ export default async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     /*
-     * Match all request paths except for the ones starting with:
+     * Match all paths except:
+     * - api/stripe/webhook (Stripe webhooks)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
+     * - public files (images, etc)
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api/stripe/webhook|_next/static|_next/image|favicon.ico|.*\\.(svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }; 
